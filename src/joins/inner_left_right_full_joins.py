375654d540aeb051d037dcaa41dfa8a324e1cbf7
DBT-Stewart <stewartprince.PM@diggibyte.com>
2025-07-07 15:23:40 +0530

    practiced on joins like inner, right, left and full

---
+++ src/joins/inner_left_right_full_joins.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("BasicJoinsExample").getOrCreate()
+
+data_1 = [(1,'Stewart',21,1),(2,'Pooja',19,2),(3,'Hema',23,1),(4,'Sushmitha',22,2),(5,'Edwin',25,3),(6,'Aishu',20,4)]
+schema_1 = ['id', 'name', 'age', 'branch_id']
+
+data_2 = [(1,'Hosur'),(2,'Bangalur'),(5,'Swedan')]
+schema_2 = ['br_id','br_name']
+
+empDf = spark.createDataFrame(data_1,schema_1)
+brDf = spark.createDataFrame(data_2,schema_2)
+
+empDf.show()
+brDf.show()
+
+print("Inner Join :")
+empDf.join(brDf, empDf.branch_id == brDf.br_id, 'inner').show()
+print("Right Join :")
+empDf.join(brDf, empDf.branch_id == brDf.br_id, 'right').show()
+print("Left Join :")
+empDf.join(brDf, empDf.branch_id == brDf.br_id, 'left').show()
+print("Full Join :")
+empDf.join(brDf, empDf.branch_id == brDf.br_id, 'full').show()
+
