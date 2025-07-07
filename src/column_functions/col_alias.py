5dbc2f17916268ee14f383691477657f098d3d76
DBT-Stewart <stewartprince.PM@diggibyte.com>
2025-07-07 12:28:10 +0530

    practiced on function to display a particular column with the new specified name and data using alias()

---
+++ src/column_functions/col_alias.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("AliasExample").getOrCreate()
+
+data = [(1,'Stewart',20),(2,'Pooja',19),(3,'Edwin','52')]
+schema = ['id', 'name', 'age']
+
+df = spark.createDataFrame(data, schema)
+df.show()
+
+df.select(df.name.alias('emp_name')).show()
